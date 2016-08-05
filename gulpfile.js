15fd0fad2fb0c552947380e303ce81f1919f7553
pedroramus <p.i.c.m.ramos@gmail.com>
2016-08-04 22:34:25 -0300

    adding gulp to automatize sass files

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+
+var paths = {
+    sass: ['./lib/scss/*.scss']
+};
+
+gulp.task('default', ['watch']);
+
+gulp.task('sass', function(done) {
+    gulp.src('./lib/scss/main.scss')
+        .pipe(sass())
+        .on('error', sass.logError)
+        .pipe(minifyCss({
+            keepSpecialComments: 0
+        }))
+        .pipe(rename('styles.min.css'))
+        .pipe(gulp.dest('./assets/css/'))
+        .on('end', done);
+});
+
+gulp.task('watch', function() {
+    gulp.watch(paths.sass, ['sass']);
+});
